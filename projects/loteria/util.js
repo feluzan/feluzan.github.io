544b97e28c7b865463b16d2c4f41362129ed533e
Félix Zanetti <feluzan@gmail.com>
2023-12-15 10:16:24 -0300

    Update util.js
    Alterando rota da api

--- projects/loteria/util.js
+++ projects/loteria/util.js
@@ -20,25 +20,25 @@
     });
 }
 
-async function getResult(idConcurso, tipoLoteria="mega-sena"){
+async function getResult(idConcurso, tipoLoteria="megasena"){
     var ret = await makeGetRequest('https://loteriascaixa-api.herokuapp.com/api/' + tipoLoteria + '/' + idConcurso);
     return ret;
     
 }
 
-async function getLatest(tipoLoteria = "mega-sena"){
+async function getLatest(tipoLoteria = "megasena"){
     var ret = await makeGetRequest('https://loteriascaixa-api.herokuapp.com/api/' + tipoLoteria + '/latest');
     // console.log(ret);
     return ret;
 }
 
-async function getAll(tipoLoteria = "mega-sena"){
+async function getAll(tipoLoteria = "meg-sena"){
     var ret = await makeGetRequest('https://loteriascaixa-api.herokuapp.com/api/' + tipoLoteria);
 
     return ret;
 }
 
-async function getAllFetched(tipoLoteria = "mega-sena"){
+async function getAllFetched(tipoLoteria = "megasena"){
     var prom = getAll(tipoLoteria);
     var aux = prom.then(value => {
         return value;
